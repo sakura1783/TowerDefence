f7682fdd8a6313b751af32a5af8a07f5a6452e74
sakura1783 <sarukaniumai@gmail.com>
2023-06-02 21:52:08 +0900

    ステージ選択用ポップアップの処理作成途中

--- Assets/Scripts/StageButtons.cs
+++ Assets/Scripts/StageButtons.cs
@@ -5,8 +5,33 @@ using UnityEngine.UI;
 
 public class StageButtons : MonoBehaviour
 {
-    public void OnClickStageButton()
-    {
+    [SerializeField] private Button stageButton;
 
+    [SerializeField] private Button btnEnter;
+
+    [SerializeField] private PlacementSelectStagePopUp placementSelectStagePopUp;
+
+    [SerializeField] private StageData stageData;
+
+    public void SetUpStageButtons()
+    {
+        stageButton.onClick.AddListener(OnClickStageButton);
+        btnEnter.onClick.AddListener(OnClickEnter);
+    }
+
+    /// <summary>
+    /// ステージ選択のボタンを押した際の処理
+    /// </summary>
+    private void OnClickStageButton()
+    {
+        placementSelectStagePopUp.SetUpPlacementSelectStagePopUp(stageData);
+    }
+
+    /// <summary>
+    /// 「ゲームを開始する」ボタンを押した際の処理
+    /// </summary>
+    private void OnClickEnter()
+    {
+        //TODO それぞれのステージに飛ぶ
     }
 }
